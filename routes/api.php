f1d9b1004e8d36e9188a62885eff80b299e62218
cor <m.masong14@gmail.com>
2024-07-05 10:33:27 +0800

    configuring api requirements

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CommentController;
+
+Route::middleware('auth:sanctum')->group(function () {
+ Route::apiResource('posts', PostController::class);
+ Route::post('comments', [CommentController::class, 'store']);
+ Route::put('comments/{comment}', [CommentController::class, 'update']);
+ Route::delete('comments/{comment}', [CommentController::class,'destroy']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
